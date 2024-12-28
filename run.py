abd1d1e76f6df456a8be99fe63e2d73db5a3a02f
Daniel Elde-Johansson <biz45dy2@students.codeinstitute.net>
2024-12-28 08:27:40 +0000

    Created a board

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+"""
+Created an empty board
+"""
+
+def create_board():
+    board = []
+    for _ in range(10):
+        row = [' '] * 10
+        board.append(row)
+    return board
+
