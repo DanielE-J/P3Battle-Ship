07f6ce813f5e6d31852f61411980893bba95d351
Daniel Elde-Johansson <biz45dy2@students.codeinstitute.net>
2024-12-28 08:29:30 +0000

    Printed a 10x10 board

--- run.py
+++ run.py
@@ -10,3 +10,13 @@ def create_board():
         board.append(row)
     return board
 
+"""
+Function to print the board
+"""
+
+def print_board(board):
+    print('   A B C D E F G H I J')
+    print('  -------------------')
+    for i in range(10):
+        print(f'{j+1} |{"|".join(board[j])}|')
+        print('  -------------------')
